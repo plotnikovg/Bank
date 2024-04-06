8ff1e8cdab32df04e56ec78e50981a18607b7bf7
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-06 20:43:47 +0300

    Added Passport Entity

---
+++ Bank.Domain/Aggregates/ClientAggregate/Passport.cs
@@ -0,0 +1,23 @@
+namespace Bank.Domain.Aggregates.ClientAggregate;
+
+public class Passport
+{
+    //public Name Name { get; private set; }
+    public string Series { get; private set; }
+    public string Number { get; private set; }
+    public RegistrationAddress RegistrationAddress { get; private set; }
+
+    public Passport(string series, string number, RegistrationAddress registrationAddress)
+    {
+        if (!IsSeriesValid(series))
+            throw new ArgumentException("series is not valid");
+        if (!IsNumberValid(number))
+            throw new ArgumentException("number is not valid");
+        Series = series;
+        Number = number;
+        RegistrationAddress = registrationAddress;
+    }
+
+    private bool IsSeriesValid(string series) => series.Length == 4 ? true : false;
+    private bool IsNumberValid(string number) => number.Length == 6 ? true : false;
+}
--- Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
+++ Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86b29c862dcdc3ea03f9d5a8b36cd2dae00dcd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d980fa49386d2805e6b933cb696faff717f28ae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
