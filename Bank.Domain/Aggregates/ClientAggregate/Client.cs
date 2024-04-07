5e200f7bfe8c0db8e9e9aff2f28108f3951ef873
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-07 20:06:12 +0300

    Changed "BaseEntity" Id type field

--- Bank.Domain/Aggregates/ClientAggregate/Client.cs
+++ Bank.Domain/Aggregates/ClientAggregate/Client.cs
@@ -3,9 +3,8 @@ using Bank.Domain.Common;
 namespace Bank.Domain.Aggregates.ClientAggregate;
 
 //Bank client
-public class Client : IAggregateRoot
+public class Client : BaseEntity, IAggregateRoot
 {
-    public Guid Id { get; private set; }
     public Name Name { get; private set; }
     
 }
--- Bank.Domain/Common/BaseEntity.cs
+++ Bank.Domain/Common/BaseEntity.cs
@@ -2,7 +2,7 @@ namespace Bank.Domain.Common;
 
 public abstract class BaseEntity
 {
-    public int Id { get; set; }
+    public Guid Id { get; private set; }
 
     private readonly List<BaseEvent> _domainEvents = [];
 
