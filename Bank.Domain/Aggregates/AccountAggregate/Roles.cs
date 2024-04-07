901ff476be86e7b1bdaad8155f8d635938397ade
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-07 19:54:13 +0300

    Added Roles class

---
+++ Bank.Domain/Aggregates/AccountAggregate/Roles.cs
@@ -0,0 +1,8 @@
+namespace Bank.Domain.Aggregates.AccountAggregate;
+
+public abstract class Roles
+{
+    public const string Client = nameof(Client);
+    public const string Manager = nameof(Client);
+    public const string Admin = nameof(Admin);
+}
--- Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
+++ Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d980fa49386d2805e6b933cb696faff717f28ae1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1955a8103591251e01b78c21f46ebd239abdb3c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
