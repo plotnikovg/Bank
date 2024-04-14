37010b6ea03050cd6e301836ae2b0134241a8797
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-14 14:19:40 +0300

    added properties

--- Bank.Domain/Aggregates/BankAccountAggregate/BankAccount.cs
+++ Bank.Domain/Aggregates/BankAccountAggregate/BankAccount.cs
@@ -4,5 +4,7 @@ namespace Bank.Domain.Aggregates.BankAccountAggregate;
 
 public class BankAccount : BaseEntity, IAggregateRoot
 {
-    
+    public Money Balance { get; private set; }
+    public decimal WithdrawalLimit { get; private set; } //Лимит на снятие средств
+    public List<BankCard> BankCards { get; private set; }  //Карты
 }
