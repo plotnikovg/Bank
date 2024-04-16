5738363a64ee6472de2ef27d085562b8026b59e3
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-16 13:32:18 +0300

    Added some domain logic

--- Bank.Domain/Aggregates/BankAccountAggregate/BankAccount.cs
+++ Bank.Domain/Aggregates/BankAccountAggregate/BankAccount.cs
@@ -6,5 +6,36 @@ public class BankAccount : BaseEntity, IAggregateRoot
 {
     public Money Balance { get; private set; }
     public decimal WithdrawalLimit { get; private set; } //Лимит на снятие средств
-    public List<BankCard> BankCards { get; private set; }  //Карты
+    //BankCards items can be added only through AddBankCard method
+    private readonly List<BankCard> _bankCards;
+    public IReadOnlyCollection<BankCard> BankCards => _bankCards.AsReadOnly(); //Карты
+
+    protected BankAccount()
+    {
+        _bankCards = new List<BankCard>();
+    }
+
+public BankAccount(Money balance, decimal withdrawalLimit)
+    {
+        Balance = balance;
+        WithdrawalLimit = withdrawalLimit;
+    }
+
+    public void AddBankCard()
+    {
+        _bankCards.Add(new BankCard());
+    }
+
+    public bool IsBalanceNegative() => Balance.Amount < 0;
+    public void BalanceIncrease(Money money)
+    {
+        if (Balance.Currency != money.Currency) throw new InvalidOperationException();
+        Balance.Increase(money.Amount);
+    }
+
+    public void BalanceDecrease(Money money)
+    {
+        if (Balance.Currency != money.Currency) throw new InvalidOperationException();
+        Balance.Decrease(money.Amount);
+    }
 }
