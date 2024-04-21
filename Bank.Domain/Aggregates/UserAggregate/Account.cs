32cf54bfc5fd3f647c5ce2311ecce194745ca9d6
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-21 13:38:26 +0300

    implement AggregateRoot

--- Bank.Domain/Aggregates/UserAggregate/Account.cs
+++ Bank.Domain/Aggregates/UserAggregate/Account.cs
@@ -8,10 +8,34 @@ public class Account : BaseEntity, IAggregateRoot
     [Required]
     public string Login { get; private set; }
     public string Password { get; private set; }
+    public Roles Role { get; private set; }
 
-    public Account(string login, string password)
+    public Account(string login, string password, Roles role)
     {
         Login = login;
         Password = password;
+        Role = role;
+    }
+
+    public Account CreateAdminAccount(string login,
+        string password)
+    {
+        return new Account(login,
+            password,
+            Roles.Admin);
+    }
+    public Account CreateManagerAccount(string login,
+        string password)
+    {
+        return new Account(login,
+            password,
+            Roles.Manager);
+    }
+    public Account CreatClientAccount(string login,
+        string password)
+    {
+        return new Account(login,
+            password,
+            Roles.Client);
     }
 }
