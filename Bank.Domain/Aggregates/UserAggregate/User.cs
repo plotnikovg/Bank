0c77bb7d421ed62bef1983862fee62f31fea3510
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-23 18:00:56 +0300

    remove public constructor

--- Bank.Domain/Aggregates/UserAggregate/User.cs
+++ Bank.Domain/Aggregates/UserAggregate/User.cs
@@ -10,28 +10,32 @@ public class User : BaseEntity, IAggregateRoot
     public string Password { get; private set; }
     public Roles Role { get; private set; }
 
-    public User(string login, string password, Roles role)
+    protected User()
+    {
+        
+    }
+    private User(string login, string password, Roles role)
     {
         Login = login;
         Password = password;
         Role = role;
     }
 
-    public User CreateAdminAccount(string login,
+    public static User CreateAdminAccount(string login,
         string password)
     {
         return new User(login,
             password,
             Roles.Admin);
     }
-    public User CreateManagerAccount(string login,
+    public static User CreateManagerAccount(string login,
         string password)
     {
         return new User(login,
             password,
             Roles.Manager);
     }
-    public User CreatClientAccount(string login,
+    public static User CreatClientAccount(string login,
         string password)
     {
         return new User(login,
