cde4c6f144d90e9ba11fd7eb1b1c9d0552dd6142
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-23 17:45:59 +0300

    initial

---
+++ Bank.Infrastructure/BankContext.cs
@@ -0,0 +1,15 @@
+using Bank.Domain.Aggregates.BankAccountAggregate;
+using Bank.Domain.Aggregates.ClientAggregate;
+using Bank.Domain.Aggregates.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bank.Infrastracture;
+
+public class BankContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<User> Users { get; set; }
+    
+    public BankContext(DbContextOptions<BankContext> options) : base(options) { }
+}
