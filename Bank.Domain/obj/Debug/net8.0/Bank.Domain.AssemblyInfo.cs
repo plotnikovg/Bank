ee19df3d7d2475f01714c1eb22e5d236e1b49ccf
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-25 12:55:44 +0300

    assembly

--- Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
+++ Bank.Domain/obj/Debug/net8.0/Bank.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f42a02e6b21312d2e8c069da821ce2687588566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6438382252bac7f7934d127d46d8b4e1bbd271d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
