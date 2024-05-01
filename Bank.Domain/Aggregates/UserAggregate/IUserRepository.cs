b90077f30f266f8464b1688bebe796efa7ee61dd
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-01 17:23:20 +0300

    Change FindAsync result to possible null value

--- Bank.Domain/Aggregates/UserAggregate/IUserRepository.cs
+++ Bank.Domain/Aggregates/UserAggregate/IUserRepository.cs
@@ -4,6 +4,6 @@ public interface IUserRepository
 {
     User Add(User user);
     User Update(User user);
-    Task<User> FindAsync(string login);
+    Task<User?> FindAsync(string login);
     Task<User> FindByIdAsync(Guid id);
 }
