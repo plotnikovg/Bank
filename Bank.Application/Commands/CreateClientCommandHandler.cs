173a14fb47026823033aeecc0f1c2bb05dde0a21
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-05 19:03:15 +0300

    Init commit

---
+++ Bank.Application/Commands/CreateClientCommandHandler.cs
@@ -0,0 +1,30 @@
+using Bank.Domain.Aggregates.ClientAggregate;
+
+namespace Bank.Application.Commands;
+
+public class CreateClientCommandHandler : IRequestHandler<CreateClientCommand, bool>
+{
+    private readonly IClientRepository _clientRepository;
+    private readonly ILogger<CreateClientCommand> _logger;
+
+    public CreateClientCommandHandler(IClientRepository clientRepository, ILogger<CreateClientCommand> logger)
+    {
+        _clientRepository = clientRepository ?? throw new ArgumentNullException(nameof(clientRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+    
+    public async Task<bool> Handle(CreateClientCommand request, CancellationToken cancellationToken)
+    {
+        var name = new Name(request.FirstName, request.LastName, request.Patronymic);
+        var registrationAddress = new RegistrationAddress(request.RegistrationDate, request.Region, request.City,
+            request.Street, request.HouseNumber, request.BuildingNumber);
+        var passport = new Passport(name, request.PassportSeries, request.PassportNumber, registrationAddress);
+        var client = new Client(passport, request.PhoneNumber);
+
+        _logger.LogInformation("Create client: {@client}", client);
+
+        _clientRepository.Add(client);
+
+        return await _clientRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+    }
+}
