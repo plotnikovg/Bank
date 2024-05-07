48cc2595f0e62113e10db06c13f91b7879cca568
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-07 11:59:13 +0300

    useless commit

--- Bank.Application/Common/GlobalUsing.cs
+++ Bank.Application/Common/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using Microsoft.Extensions.Logging;
 
 global using Bank.Domain.Aggregates.ClientAggregate;
 global using Bank.Infrastructure.Repositories;
+//global using System.Linq;
+//global using System.Collections.Generic;
