b8ebd0cddfda3ee37b37c3cbb9e01e52e79d808c
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-07 13:27:16 +0300

    Add BankAccountAggregate

--- Bank.Infrastructure/GlobalUsing.cs
+++ Bank.Infrastructure/GlobalUsing.cs
@@ -1,4 +1,5 @@
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Bank.Domain.Aggregates.ClientAggregate;
+global using Bank.Domain.Aggregates.BankAccountAggregate;
 global using Bank.Domain.Aggregates.UserAggregate;
