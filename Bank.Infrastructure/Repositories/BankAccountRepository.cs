fedfb6d5d8ccfacd067f0ad445908b19cf12cf17
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-07 14:00:04 +0300

    Init commit

---
+++ Bank.Infrastructure/Repositories/BankAccountRepository.cs
@@ -0,0 +1,49 @@
+using Bank.Domain.Common;
+
+namespace Bank.Infrastructure.Repositories;
+
+public class BankAccountRepository : IBankAccountRepository
+{
+    private readonly BankContext _context;
+    public IUnitOfWork UnitOfWork => _context;
+
+    public BankAccountRepository(BankContext context)
+    {
+        _context = context;
+    }
+    public BankAccount Add(BankAccount bankAccount)
+    {
+        return _context.BankAccounts
+            .Add(bankAccount)
+            .Entity;
+    }
+
+    public BankAccount Update(BankAccount bankAccount)
+    {
+        return _context.BankAccounts
+            .Update(bankAccount)
+            .Entity;
+    }
+
+    public async Task<bool> DeleteByIdAsync(Guid id)
+    {
+        var bankAccount = await FindByIdAsync(id) ?? throw new ArgumentNullException(nameof(BankAccount), "BankAccount not found");
+        var a = _context.BankAccounts
+            .Remove(bankAccount);
+        return true;
+    }
+
+    public async Task<bool> DeleteAsync(BankAccount bankAccount)
+    {
+        var a =  _context.BankAccounts
+            .Remove(bankAccount);
+        return true;
+    }
+
+    public async Task<BankAccount?> FindByIdAsync(Guid id)
+    {
+        var bankAccount = await _context.BankAccounts
+            .FindAsync(id);
+        return bankAccount;
+    }
+}
