f149f67f1c11b4beab9539862fadd1f3490004c9
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-07 11:22:55 +0300

    Change parameter name to "phoneNumber"

--- Bank.Domain/Aggregates/ClientAggregate/IClientRepository.cs
+++ Bank.Domain/Aggregates/ClientAggregate/IClientRepository.cs
@@ -5,7 +5,7 @@ public interface IClientRepository
     IUnitOfWork UnitOfWork { get; }
     Client Add(Client client);
     Client Update(Client client);
-    Task<Client?> FindByPhoneNumberAsync(string login);
+    Task<Client?> FindByPhoneNumberAsync(string phoneNumber);
     Task<Client?> FindByPassportAsync(Passport passport);
     Task<Client> FindByIdAsync(Guid id);
 }
