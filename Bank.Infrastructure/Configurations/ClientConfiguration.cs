978d0ffa909574b59d2ccadf99a0ebade6f15dfa
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-08 17:38:18 +0300

    Fix relationships with "Passport"

--- Bank.Infrastructure/Configurations/ClientConfiguration.cs
+++ Bank.Infrastructure/Configurations/ClientConfiguration.cs
@@ -7,9 +7,20 @@ public class ClientConfiguration : IEntityTypeConfiguration<Client>
         clientConfiguration.ToTable("Clients");
         clientConfiguration.Ignore(x => x.DomainEvents);
         clientConfiguration.Ignore(p => p.Name);
-        clientConfiguration.OwnsOne(p => p.Passport).OwnsOne(p => p.Name);
-        clientConfiguration.OwnsOne(p => p.Passport).OwnsOne(p => p.RegistrationAddress);
-            
+        // clientConfiguration.OwnsOne(p => p.Passport).OwnsOne(p => p.Name);
+        // clientConfiguration.OwnsOne(p => p.Passport).OwnsOne(p => p.RegistrationAddress);
+        // clientConfiguration.OwnsOne(p => p.Passport, a =>
+        // {
+        //     a.OwnsOne(o => o.Name);
+        //     a.OwnsOne(o => o.RegistrationAddress);
+        // });
+        clientConfiguration.OwnsOne(p => p.Passport, n =>
+        {
+            n.OwnsOne(o => o.Name);
+            n.OwnsOne(o => o.RegistrationAddress);
+        });
+        
+
         //clientConfiguration.OwnsMany() TODO
     }
 }
