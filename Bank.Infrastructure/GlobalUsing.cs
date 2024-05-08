3512b77c57a28e78db5d24f384d9832cfad64895
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-08 14:26:59 +0300

    Add mediatr

--- Bank.Infrastructure/GlobalUsing.cs
+++ Bank.Infrastructure/GlobalUsing.cs
@@ -1,5 +1,8 @@
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Design;
+
+global using MediatR;
 global using Bank.Domain.Aggregates.ClientAggregate;
 global using Bank.Domain.Aggregates.BankAccountAggregate;
 global using Bank.Domain.Aggregates.UserAggregate;
