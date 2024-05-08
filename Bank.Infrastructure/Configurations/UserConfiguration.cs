6d9791233a537c488d4ec99d6d8403494286a765
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-08 14:26:39 +0300

    Add Role configuration

--- Bank.Infrastructure/Configurations/UserConfiguration.cs
+++ Bank.Infrastructure/Configurations/UserConfiguration.cs
@@ -8,6 +8,10 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         userConfiguration.Ignore(x => x.DomainEvents);
         userConfiguration.Property(o => o.Login).IsRequired();
         userConfiguration.Property(o => o.Password).IsRequired();
-        userConfiguration.OwnsOne(p => p.Role);
+        userConfiguration.OwnsOne(p => p.Role, a =>
+        {
+            a.Property(u => u.Value).HasColumnName("Role");
+            a.Property(u => u.Value).HasColumnType("nvarchar(20)");
+        });
     }
 }
