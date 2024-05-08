babe8ad0baaa2dd07b667a8c42422fc8fe8c3e51
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-08 17:10:02 +0300

    Add migration

--- Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
+++ Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb1a4ab642c2769275a072c8449efc5845582dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72729e15f3ceb787bb4784ff457b3d94bb659526")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
