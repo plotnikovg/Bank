e801ccbba70069e734d5079ab19f0698da7651ee
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-08 17:38:47 +0300

    Add data seeding into comment

--- Bank.Infrastructure/BankContext.cs
+++ Bank.Infrastructure/BankContext.cs
@@ -26,10 +26,33 @@ public class BankContext : DbContext, IUnitOfWork
         //TODO
         modelBuilder.ApplyConfiguration(new ClientConfiguration());
         modelBuilder.ApplyConfiguration(new BankAccountConfiguration());
+        // modelBuilder.Entity<Client>()
+        //     .HasData(
+        //         new Client(
+        //             new Passport(
+        //                 new Name("Иван", "Петрович", "Иванов", "Иванович"),
+        //                 "1234",
+        //                 "567890",
+        //                 new RegistrationAddress(
+        //                     new DateOnly(2000, 1, 1),
+        //                     "Москва",
+        //                     "Москва",
+        //                     "Улица",
+        //                     1,
+        //                     "2")), 
+        //             "+7(999)999-99-99")
+        //     );
         // modelBuilder.Entity<Client>().HasData(
-        //     new Client(new Passport(new Name("Сергей","Михаил", "Иванов", "Михайлович"), "1234", "22226666",
-        //             new RegistrationAddress(new DateOnly(2003, 6, 18), "-", "Москва", "Комсомольская", 27)),
-        //         "+7(999)999-99-99"));
+        //     new {Id = "9e677d52-8a3f-4bb7-b488-b0c8ccf644fa", PhoneNumber = "+7(999)999-99-99"});
+        // modelBuilder.Entity<Client>().OwnsOne(p => p.Passport).HasData(
+        //     new {ClientId = "9e677d52-8a3f-4bb7-b488-b0c8ccf644fa", Series = "1234", Number = "567890" });
+        // modelBuilder.Entity<Client>().OwnsOne(p => p.Passport).OwnsOne(n => n.Name).HasData(
+        //     new
+        //     {
+        //         ClientId = "9e677d52-8a3f-4bb7-b488-b0c8ccf644fa", FirstName = "Иван", SecondName = "Петрович",
+        //         LastName = "Иванов", MiddleName = "Иванович"
+        //     }
+        // );
     }
 
     public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
