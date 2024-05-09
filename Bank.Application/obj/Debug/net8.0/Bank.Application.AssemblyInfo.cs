1a9ba79dcb6e86290ea99c7b949fec4aef047c4d
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-09 12:59:30 +0300

    Migration

--- Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
+++ Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7397e4128e3f190a391b5b450b7997e412f2f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f9658454fb139615fd7e23f8c22ec053bd94c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
