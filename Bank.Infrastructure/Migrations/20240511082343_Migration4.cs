14e20703a6938c278eed7b09ac964b9a564a6ff9
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-11 11:31:24 +0300

    Init commit

---
+++ Bank.Infrastructure/Migrations/20240511082343_Migration4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bank.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Balance_Currency_Code",
+                table: "BankAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance_Currency_Code",
+                table: "BankAccounts");
+        }
+    }
+}
---
+++ Bank.Infrastructure/Migrations/20240511082751_Migration5.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bank.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Balance_Currency_Code",
+                table: "BankAccounts",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Balance_Currency_Code",
+                table: "BankAccounts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+        }
+    }
+}
