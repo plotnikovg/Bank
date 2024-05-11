68c13784fbf92d6a49a3911e64a22bf914e111b1
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-11 12:04:44 +0300

    Add identity

--- Bank.Infrastructure/GlobalUsing.cs
+++ Bank.Infrastructure/GlobalUsing.cs
@@ -1,6 +1,7 @@
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore.Design;
+global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 global using MediatR;
