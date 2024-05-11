d8d3569bf42cc80968ea35b8e60becebcf44aed9
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-11 12:26:17 +0300

    Init commit

---
+++ Bank.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using System.Runtime.Versioning;
+using Bank.Infrastructure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bank.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<BankContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<BankContext>();
+        
+        return services;
+    }
+}
