038e22c584b03607633fba21f7ba64ab049b5360
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-14 19:57:06 +0300

    assembly

--- Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
+++ Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aea550b2dc3a901716bd938ef491f936b12eefc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a37b9cbe6d962ee6cd76e51dabc026a826ce789b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
