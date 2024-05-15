f0d49323c701487d000e188cc45f101550ad0e13
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-15 16:20:59 +0300

    Init commit

---
+++ Bank.API/Models/LoginRequest.cs
@@ -0,0 +1,11 @@
+namespace Bank.API.Models;
+/// <summary>
+/// 
+/// </summary>
+/// <param name="UserName">Может содержать как номер телефона, так и email или username</param>
+/// <param name="Password">Пароль</param>
+public class LoginRequest
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
