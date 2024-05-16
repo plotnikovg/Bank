4bbd53909b3e0b54237dbb2812930ab4e91985d4
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-16 17:00:17 +0300

    Delete infrastructure reference

--- Bank.Application/Common/GlobalUsing.cs
+++ Bank.Application/Common/GlobalUsing.cs
@@ -2,6 +2,5 @@ global using MediatR;
 global using Microsoft.Extensions.Logging;
 
 global using Bank.Domain.Aggregates.ClientAggregate;
-global using Bank.Infrastructure.Repositories;
 //global using System.Linq;
 //global using System.Collections.Generic;
--- Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
+++ Bank.Application/obj/Debug/net8.0/Bank.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7cdf335846f1f2af40b26b0c666913ace3307b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b680375e113fcfcacdb336731cc7a23e135a3da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
