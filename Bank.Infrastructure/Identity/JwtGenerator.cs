d3e7896dcd3861cf1c3081955d5caba5d1f62a46
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-24 19:33:28 +0300

    Change key

--- Bank.Infrastructure/Identity/JwtGenerator.cs
+++ Bank.Infrastructure/Identity/JwtGenerator.cs
@@ -14,7 +14,7 @@ public class JwtGenerator : IJwtGenerator
 
     public JwtGenerator(IConfiguration configuration)
     {
-        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("test token key"));
+        _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hru3j7fz2k91k7g467dgte543mnf7g4f8h9fj7ydfg6789dfgdhfkghdfg67823p29375khdfg6"));
     }
     public string CreateToken(IdentityUser identityUser)
     {
