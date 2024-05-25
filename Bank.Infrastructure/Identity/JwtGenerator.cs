4d633fdb6b27e4db2f66577337c7cfb1f299e684
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-25 11:57:42 +0300

    Add roles claim

--- Bank.Infrastructure/Identity/JwtGenerator.cs
+++ Bank.Infrastructure/Identity/JwtGenerator.cs
@@ -16,9 +16,14 @@ public class JwtGenerator : IJwtGenerator
     {
         _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("hru3j7fz2k91k7g467dgte543mnf7g4f8h9fj7ydfg6789dfgdhfkghdfg67823p29375khdfg6"));
     }
-    public string CreateToken(IdentityUser identityUser)
+    public string CreateToken(IdentityUser identityUser, List<string> roles)
     {
-        var claims = new List<Claim>{new Claim(JwtRegisteredClaimNames.NameId, identityUser.UserName)};
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.NameId, identityUser.Id)
+        };
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
 
         var credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
 
