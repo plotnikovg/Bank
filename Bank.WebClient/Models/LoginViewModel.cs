8bc055c0c2ebd8dad09b417a937320a1c8fcb9e8
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-28 13:13:24 +0300

    Add "Ip" and "Browser" properties

--- Bank.WebClient/Models/LoginViewModel.cs
+++ Bank.WebClient/Models/LoginViewModel.cs
@@ -4,4 +4,6 @@ public class LoginViewModel
 {
     public string UserName { get; set; }
     public string Password { get; set; }
+    public string? Ip { get; set; } = null;
+    public string? Browser { get; set; } = null;
 }
