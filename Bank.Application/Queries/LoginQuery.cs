f9194a90c453058a4bd8ba314f94695e123ef6ba
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-28 20:35:10 +0300

    Add "Ip" and "Browser" properties

--- Bank.Application/Queries/LoginQuery.cs
+++ Bank.Application/Queries/LoginQuery.cs
@@ -11,4 +11,6 @@ public class LoginQuery : IRequest<Tuple<bool, UserViewModel>>
 {
     public string UserName { get; set; }
     public string Password { get; set; }
+    public string Ip { get; set; }
+    public string Browser { get; set; }
 }
