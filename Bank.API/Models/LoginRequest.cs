2dbbb21e35e9ac3ffa3f0861fe804f58e5a8e272
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-05-28 20:33:43 +0300

    Add "Ip" and "Browser" properties

--- Bank.API/Models/LoginRequest.cs
+++ Bank.API/Models/LoginRequest.cs
@@ -8,4 +8,6 @@ public class LoginRequest
 {
     public string UserName { get; set; }
     public string Password { get; set; }
+    public string Ip { get; set; }
+    public string Browser { get; set; }
 }
