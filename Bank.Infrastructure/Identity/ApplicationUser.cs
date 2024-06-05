edea88629f2fd4194aefef8f8721b36b77d5a373
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-06-05 20:34:00 +0300

    Remove all body

--- Bank.Infrastructure/Identity/ApplicationUser.cs
+++ Bank.Infrastructure/Identity/ApplicationUser.cs
@@ -2,11 +2,5 @@ namespace Bank.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    public ApplicationUser(string phoneNumber, string password)
-    {
-        PhoneNumber = phoneNumber;
-        Password = password;
-    }
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
+
 }
