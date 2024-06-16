f21f52e0d2c977f536afa58d412b31f9f122b7e2
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-06-16 16:40:30 +0300

    Assembly

--- Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
+++ Bank.API/obj/Debug/net8.0/Bank.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bb603006af02be3e01dcb287eedf9e21fc8a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d97b41676b64b0c9d385d75e9a71cfa8b13894")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
